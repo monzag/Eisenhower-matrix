ac8b6bcd2def8a61f36dfa4a951dce043ef6da64
monzag <zaganiacz.m@gmail.com>
2017-06-20 01:17:37 +0200

    create class TodoItem, method: init, mark, unmark, __str__, __len__

---
+++ todo_item.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+
+class TodoItem():
+
+    def __init__(self, title, deadline):
+        '''
+        Method creates an instance of the class.
+        Raise error if title is not a string and when deadline has incorrect format.
+
+        Returns:
+           None
+        '''
+
+        if not isinstance(title, str):
+            raise TypeError
+
+        elif not isinstance(deadline, datetime):
+            raise TypeError("wrong date format!")
+
+        self.title = title
+        self.deadline = deadline
+        self.is_done = False
+
+    def mark(self):
+        '''
+        Method changes parametr is_done to True.
+
+        Returns:
+            None
+        '''
+
+        self.is_done = True
+
+    def unmark(self):
+        '''
+        Method changes parametr is_done to False.
+
+        Returns:
+            None
+        '''
+
+        self.is_done = False
+
+    def __str__(self):
+        '''
+        Method returns property string, depending on parameter is_done. 
+
+        Returns:
+            string
+        '''
+
+        if self.is_done is True:
+            return '[x] ' + str(self.deadline.day) + '-' + str(self.deadline.month) + ' ' + self.title
+        else:
+            return '[ ] ' + str(self.deadline.day) + '-' + str(self.deadline.month) + ' ' + self.title
+
+    def __len__(self):
+        '''
+        Method returns length of attribute title.
+
+        Returns:
+            int
+        '''
+
+        return len(self.title)
